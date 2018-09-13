d5ffa9c073d4b7f916ab1e63969da72637b8733e
afinado <afinado@a1.net>
2018-09-13 18:45:22 +0200

    add mockito test cases

---
+++ webdemo/src/main/java/org/gradle/demo/HelloServlet.java
@@ -0,0 +1,26 @@
+package org.gradle.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"}, loadOnStartup = 1)
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().print("Hello, World!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name == null || name.isEmpty()) {
+            name = "World";
+        }
+        request.setAttribute("user", name);
+        request.getRequestDispatcher("response.jsp").forward(request, response);
+    }
+}
